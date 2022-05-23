21534ecf9f8b356795da6b97c485f43f80465de5
jean-leal <jeanlds13@gmail.com>
2022-05-23 11:45:52 -0300

    Primeira aula estruturas de dados

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+const somosCasados = true;
+const anoNascimento = 1996;
+const media = (7+10+5+8)/4;
+const nome = "Jean";
+let idade = null;
+const filho;
+const numeroGrande = 23243213253614536n;
+const numeroGrande2 = BigInt("3213213213465");
+const meuSimbolo = new Symbol();
+const meuObjeto= {
+  curso: "Estrutura de Dados", 
+  instrutores: ['Gabriel', 'Vanessa']
+}
